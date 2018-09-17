072193db5a1f0652c35c178d1ba2b4cdeefbb46b
SugioNakazawa <nakazawa@nautilus-technologies.com>
2018-09-17 19:50:35 +0900

    unlatch項目を[]で囲んで出力。

--- src/main/java/com/hoge/DiffCsv.java
+++ src/main/java/com/hoge/DiffCsv.java
@@ -12,6 +12,7 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Set;
 import java.util.Stack;
 
 import org.apache.commons.cli.CommandLine;
@@ -29,28 +30,45 @@ import com.opencsv.CSVReader;
 public class DiffCsv {
 
 	static Logger logger = LoggerFactory.getLogger(DiffCsv.class);
-	static boolean SHOW_DETAIL = true;
 	static String DELIMITTER = "\t";
-	String expect;
-	String result;
-	String batch;
-	String data;
-
+	private boolean showDetail;
+	private String expect;
+	private String result;
+	private String batch;
+	private String data;
+	private int excludeTargetCol;
+	private String excludeTargetValue;
+	/** データ属性保管マップ **/
+	private HashMap<String, DataAttr> dataAttrMap;
 	// 結果カウント
 	private int unMatchCount;
-	private int notFoundCount;
+	private int notFoundResCount;
 	private int matchCount;
-	private HashMap<String, DataAttr> dataAttrMap;
+	private int notFoundExpCount;
 
 	public static void main(String[] args) throws ParseException {
 		logger.info("start...");
 		long startTime = (new Date()).getTime();
-//		String[] btse001Param = { //
-//				"-e", "/Users/nakazawasugio/qd-bts/ee/btse001dp/OUT", //
-//				"-r", "/Users/nakazawasugio/qd-bts/ee/btse001dp/btse001dp/result", //
+//		String[] testParam = { //
+//				"-e", "/Volumes/GoogleDrive/マイドライブ/九電託送/Phase3/現新比較/BTS-E001/OUT", //
+//				"-r", "/Volumes/GoogleDrive/マイドライブ/九電託送/Phase3/現新比較/BTS-E001/comp_test/btse001dp/result/btsc_yuko_kwh_l_ext", //
 //				"-b", "btse001dp", //
-//				"-d", "BTSC_YUKO_KWH_L_EXT" };
-//		args = btse001Param;
+//				"-d", "BTSC_YUKO_KWH_L_EXT", //
+//				"-targetCol", "110", //
+//				"-targetValue", "BTS-E001", //
+//				"-l", "false", //
+//		};
+//		args = testParam;
+//		String[] testParam = { //
+//				"-e", "/Users/nakazawasugio/qd-bts/ee/btsh002dp/OUT", //
+//				"-r", "/Users/nakazawasugio/qd-bts/ee/btsh002dp/btsh002dp/result/btsc_yuko_kwh_l_ext", //
+//				"-b", "btsh002dp", //
+//				"-d", "BTSC_YUKO_KWH_L_EXT", //
+//				"-targetCol", "4", //
+//				"-targetValue", "2018-08-30", //
+//				"-l", "false", //
+//		};
+//		args = testParam;
 		// command line
 		Option expDirOption = Option.builder("e").longOpt("expect").hasArg().required(true).type(String.class)
 				.desc("expect direcotory base path(upper on OUT)").build();
@@ -60,61 +78,80 @@ public class DiffCsv {
 				.desc("batch name").build();
 		Option dataNameOption = Option.builder("d").longOpt("data").hasArg().required(true).type(String.class)
 				.desc("data name").build();
+		Option targetColOption = Option.builder("targetCol").longOpt("targetCol").hasArg().required(false)
+				.type(String.class).desc("exclude column id").build();
+		Option targetValueOption = Option.builder("targetValue").longOpt("targetValue").hasArg().required(false)
+				.type(String.class).desc("exclude value").build();
+		Option detailOption = Option.builder("l").longOpt("log").hasArg().required(false).type(Boolean.class)
+				.desc("show detail log").build();
 
 		Options options = new Options();
 		options.addOption(expDirOption);
 		options.addOption(resDirOption);
 		options.addOption(batchNameOption);
 		options.addOption(dataNameOption);
+		options.addOption(targetColOption);
+		options.addOption(targetValueOption);
+		options.addOption(detailOption);
 
 		try {
 			CommandLineParser parser = new DefaultParser();
 			System.out.println(String.join(",", args));
 			CommandLine commandLine = parser.parse(options, args);
-	
+
 			DiffCsv diffCsv = new DiffCsv();
 			diffCsv.expect = (String) commandLine.getParsedOptionValue("expect");
 			diffCsv.result = (String) commandLine.getParsedOptionValue("result");
 			diffCsv.batch = (String) commandLine.getParsedOptionValue("batch");
 			diffCsv.data = (String) commandLine.getParsedOptionValue("data");
+			diffCsv.excludeTargetCol = commandLine.hasOption("targetCol")
+					? Integer.parseInt((String) commandLine.getParsedOptionValue("targetCol"))
+					: -1;
+			diffCsv.excludeTargetValue = commandLine.hasOption("targetValue")
+					? (String) commandLine.getParsedOptionValue("targetValue")
+					: null;
+			diffCsv.showDetail = commandLine.hasOption("log")
+					? Boolean.parseBoolean((String) commandLine.getParsedOptionValue("log"))
+					: true;
+//			diffCsv.showDetail = Boolean.parseBoolean((String) commandLine.getOptionValue("log", "true"));
 
 			diffCsv.exec();
 		} catch (IOException ioe) {
 			ioe.printStackTrace();
-		}catch(Exception e) {
+		} catch (Exception e) {
 			HelpFormatter formatter = new HelpFormatter();
 			formatter.printHelp("DiffCsv", options);
-		}finally {
+			e.printStackTrace();
+		} finally {
 			logger.info("complete " + (((new Date()).getTime()) - startTime) + " ms");
 		}
 	}
 
 	public void exec() throws IOException {
-		// 期待するデータ
+		// 期待するデータ入力
 		List<String[]> expList = readCsvToList(this.expect + "/" + this.data + ".csv");
-		// 更新者:BTS-E001 以外を削除
-		if(dataAttrMap.get(data).targetUpdater != null) {
+		// 指定した更新者以外を削除
+		if (this.excludeTargetValue != null) {
 			Iterator<String[]> it = expList.iterator();
 			while (it.hasNext()) {
-				if (!dataAttrMap.get(data).targetUpdater.equalsIgnoreCase(it.next()[dataAttrMap.get(data).updaterCol])) {
+				if (!this.excludeTargetValue.equalsIgnoreCase(it.next()[this.excludeTargetCol])) {
 					it.remove();
 				}
 			}
 		}
 //		expList.stream().forEach(s->System.out.println(String.join(",",s)));
-		logger.debug("exp " + expList.size());
+		logger.debug("exp liens = " + expList.size());
 
 		// 出力結果データ
 		List<String[]> resList = generateResultData();
-//		for(String[] resList) {
-//		System.out.println(String.join("-", dat));
-//	}
 //		resList.stream().forEach(s->System.out.println(String.join(",",s)));
-		logger.debug("res " + resList.size());
+		logger.debug("res lines = " + resList.size());
 
 		compareExpToResList(expList, resList);
+		compareResToExpList(resList, expList);
 		logger.info("match  " + matchCount);
-		logger.info("notFound " + notFoundCount);
+		logger.info("notFoundRes " + notFoundResCount);
+		logger.info("notFoundExp " + notFoundExpCount);
 		logger.info("unmatch  " + unMatchCount);
 	}
 
@@ -135,6 +172,7 @@ public class DiffCsv {
 				if (expKey.equalsIgnoreCase(getKeyStr(res))) {
 					found = true;
 					if (compareData(exp, res)) {
+//						System.out.println("match\t" + String.join(DELIMITTER, exp));
 						matchCount++;
 					} else {
 						unMatchCount++;
@@ -143,9 +181,36 @@ public class DiffCsv {
 				}
 			}
 			if (!found) {
-				notFoundCount++;
-				if (SHOW_DETAIL)
-					System.out.println("not found exp -> res " + String.join(DELIMITTER, exp));
+				notFoundResCount++;
+				if (showDetail)
+					System.out.println("not found res only exp\t" + String.join(DELIMITTER, exp));
+			}
+		}
+	}
+
+	/**
+	 * リストの比較。結果ー＞期待値
+	 * 
+	 * @param resList
+	 * @param expList
+	 */
+	private void compareResToExpList(List<String[]> resList, List<String[]> expList) {
+		// expList -> resList
+		int errorCounter = 1;
+		for (String[] res : resList) {
+			boolean found = false;
+			String resKey = getKeyStr(res);
+			for (String[] exp : expList) {
+				// 存在するか？キーで比較
+				if (resKey.equalsIgnoreCase(getKeyStr(exp))) {
+					found = true;
+					break;
+				}
+			}
+			if (!found) {
+				notFoundExpCount++;
+				if (showDetail)
+					System.out.println("not found exp only res\t" + String.join(DELIMITTER, res));
 			}
 		}
 	}
@@ -158,33 +223,67 @@ public class DiffCsv {
 	 */
 	private boolean compareData(String[] exp, String[] res) {
 		boolean match = true;
-		List<Integer> excludeCols = dataAttrMap.get(data).excludeCols;
+		Set<Integer> unmatchCol = new HashSet<Integer>();
+		List<Integer> targetCols = dataAttrMap.get(data).targetCols;
 		for (int i = 0; i < exp.length; i++) {
-			if(!excludeCols.contains(i)) {
+			if (!targetCols.contains(i)) {
 				if (exp[i].equalsIgnoreCase(res[i])) {
 					// OK
 				} else {
 					if (dataAttrMap.get(data).floatCheckSet.contains(i)) {
-						if (Float.parseFloat(exp[i]) == Float.parseFloat(res[i])) {
+						float expf = 0f;
+						float resf = 0f;
+						try {
+							expf = Float.parseFloat(exp[i]);
+						} catch (NumberFormatException nfe) {
+							// default 0
+						}
+						try {
+							resf = Float.parseFloat(res[i]);
+						} catch (NumberFormatException nfe) {
+							// default 0
+						}
+						if (expf == resf) {
 							// OK
 						} else {
 							match = false;
+							unmatchCol.add(i);
 						}
 					} else {
 						// NG
 						match = false;
+						unmatchCol.add(i);
 					}
 				}
 			}
 		}
 		if (!match) {
-			if (SHOW_DETAIL) {
-				System.out.println("not match exp " + String.join(DELIMITTER, exp));
-				System.out.println("not match res " + String.join(DELIMITTER, res));
+			if (showDetail) {
+//				System.out.println("not match exp\t" + String.join(DELIMITTER, exp));
+//				System.out.println("not match res\t" + String.join(DELIMITTER, res));
+				System.out.println("not match exp\t" + getDifString(exp, unmatchCol));
+				System.out.println("not match res\t" + getDifString(res, unmatchCol));
 				System.out.println();
 			}
 		}
 		return match;
+	}
+
+	private String getDifString(String[] dats, Set<Integer> unmatchCol) {
+		StringBuffer sb = new StringBuffer();
+		for (int i = 0; i < dats.length; i++) {
+			if (i > 0) {
+				sb.append(DELIMITTER);
+			}
+			if (unmatchCol.contains(i)) {
+				sb.append("[");
+				sb.append(dats[i]);
+				sb.append("]");
+			} else {
+				sb.append(dats[i]);
+			}
+		}
+		return sb.toString();
 	}
 
 	private String getKeyStr(String[] dat) {
@@ -227,7 +326,9 @@ public class DiffCsv {
 		List<File> files = new ArrayList<>();
 
 		Stack<File> stack = new Stack<>();
-		stack.add(new File(path + "/" + data));
+//		stack.add(new File(path + "/" + data));
+		stack.add(new File(path));
+		// nakazawa
 		while (!stack.isEmpty()) {
 			File item = stack.pop();
 			if (item.isFile())
@@ -270,17 +371,24 @@ public class DiffCsv {
 	}
 
 	public DiffCsv() {
-		dataAttrMap = new HashMap<String,DataAttr>();
-		int[] key1 = { 0,1,2,3,4 };
-		Integer[] exclude1 = { 109,110,111 };
-		dataAttrMap.put("BTSC_YUKO_KWH_L_EXT", new DataAttr(key1,8,56,110,"BTS-E001",exclude1));
+		dataAttrMap = new HashMap<String, DataAttr>();
 
-		int[] key2 = { 1,2,3 };
+//		btse001dpで使うBTSC_YUKO_KWH_L_EXT
+		int[] key1 = { 0, 1, 2, 3, 4 };
+		Integer[] exclude1 = { 109, 110, 111 };
+		dataAttrMap.put("BTSC_YUKO_KWH_L_EXT", new DataAttr(key1, 8, 56, 110, "BTS-E001", exclude1));
+//		int[] key1 = { 0,1,2,3,4 };
+//		Integer[] exclude1 = {  };
+//		dataAttrMap.put("BTSC_YUKO_KWH_L_EXT", new DataAttr(key1,8,56,0,null,exclude1));
+
+//		btsh002dpで使うBTSC_YUKO_KWH_L_EXT
+		int[] key2 = { 1, 2, 3, 4 };
 		Integer[] exclude2 = { 100 };
-		dataAttrMap.put("BTS_YUKO_KWH_L_EXT", new DataAttr(key2,4,51,110,null,exclude2));
+		dataAttrMap.put("BTS_YUKO_KWH_L_EXT", new DataAttr(key2, 4, 51, 110, null, exclude2));
 	}
-	public class DataAttr{
-		//	data属性
+
+	public class DataAttr {
+		// data属性
 		// キー項目数。先頭から連続。
 		int[] keyCols;
 		HashSet<Integer> floatCheckSet;
@@ -288,22 +396,18 @@ public class DiffCsv {
 		int floatCheckStartCol;
 		// float比較終了カラム
 		int floatCheckEndCol;
-		// 結果の更新者カラム番号
-		int updaterCol;
-		// 結果の対象とする更新者
-		String targetUpdater;
 		// チェックしない最終カラムNo
-		List<Integer> excludeCols;
+		List<Integer> targetCols;
+
 		/**
 		 * 
-		 * @param keyColNum キー項目カラムColNoリスト。
+		 * @param keyColNum          キー項目カラムColNoリスト。
 		 * @param floatCheckStartCol float比較開始カラムNo
-		 * @param floatCheckEndCol float比較終了カラムNo
-		 * @param updaterCol 更新者カラムNo。
-		 * @param targetUpdater 対象更新者。
-		 * @param excludeCols 比較除外カラム。最後から連続。
+		 * @param floatCheckEndCol   float比較終了カラムNo
+		 * @param targetCols         比較除外カラム。最後から連続。
 		 */
-		public DataAttr(int[] keyCols,int floatCheckStartCol,int floatCheckEndCol,int updaterCol,String targetUpdater,Integer[] excludeCols) {
+		public DataAttr(int[] keyCols, int floatCheckStartCol, int floatCheckEndCol, int updaterCol,
+				String targetUpdater, Integer[] targetCols) {
 			floatCheckSet = new HashSet<Integer>();
 			this.floatCheckStartCol = floatCheckStartCol;
 			// float比較終了カラム
@@ -313,12 +417,8 @@ public class DiffCsv {
 			}
 			// キー項目ColNo
 			this.keyCols = keyCols;
-			// 結果の更新者カラム番号
-			this.updaterCol = updaterCol;
-			// 結果の対象とする更新者
-			this.targetUpdater = targetUpdater;
 			// チェックしない最終カラムの数
-			this.excludeCols = Arrays.asList(excludeCols);
+			this.targetCols = Arrays.asList(targetCols);
 		}
 	}
 }
