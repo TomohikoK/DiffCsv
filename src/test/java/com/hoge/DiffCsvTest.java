23fecd8197a45887d7d2b903678df9de834bb231
SugioNakazawa <nakazawa@nautilus-technologies.com>
2018-10-04 21:43:26 +0900

    IF0511データ対応。 テストケース追加。

---
+++ src/test/java/com/hoge/DiffCsvTest.java
@@ -0,0 +1,111 @@
+/**
+ * 
+ */
+package com.hoge;
+
+import java.io.IOException;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * @author nakazawasugio
+ *
+ */
+public class DiffCsvTest {
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	/**
+	 * BTS_YUKO_KWH_L_EXT Test method for {@link com.hoge.DiffCsv#exec()}.
+	 * @throws IOException
+	 */
+	@Test
+	public void testBtsYukoKwhLExt() throws IOException {
+		DiffCsv target = new DiffCsv( //
+				"src/test/resources/com/hoge/bts_yuko_kwh_l_ext/exp", // -e
+				"src/test/resources/com/hoge/bts_yuko_kwh_l_ext/res", // -r
+				"btse001dp", // -b
+				"BTS_YUKO_KWH_L_EXT", // -d
+				110, // -targetCol
+				null, // -targetValue
+				false// -l
+		);
+		target.exec();
+		Assert.assertEquals(3116, target.getMatchCount());
+		Assert.assertEquals(0, target.getUnMatchCount());
+		Assert.assertEquals(0, target.getNotFoundResCount());
+		Assert.assertEquals(0, target.getNotFoundExpCount());
+	}
+	/**
+	 * BTSC_YUKO_KWH_L_EXT Test method for {@link com.hoge.DiffCsv#exec()}.
+	 * @throws IOException
+	 */
+	@Test
+	public void testBtscYukoKwhLExt() throws IOException {
+		DiffCsv target = new DiffCsv( //
+				"src/test/resources/com/hoge/btsc_yuko_kwh_l_ext/exp", // -e
+				"src/test/resources/com/hoge/btsc_yuko_kwh_l_ext/res", // -r
+				"btse001dp", // -b
+				"BTSC_YUKO_KWH_L_EXT", // -d
+				110, // -targetCol
+				"BTS-E001", // -targetValue
+				false// -l
+		);
+		target.exec();
+		Assert.assertEquals(3274, target.getMatchCount());
+		Assert.assertEquals(0, target.getUnMatchCount());
+		Assert.assertEquals(0, target.getNotFoundResCount());
+		Assert.assertEquals(253, target.getNotFoundExpCount());
+	}
+	/**
+	 * BTSC_YUKO_KWH_L_EXT Test method for {@link com.hoge.DiffCsv#exec()}.
+	 * @throws IOException
+	 */
+	@Test
+	public void testIf0511() throws IOException {
+		DiffCsv target = new DiffCsv( //
+				"src/test/resources/com/hoge/if0511/exp", // -e
+				"src/test/resources/com/hoge/if0511/res", // -r
+				"if0511dp", // -b
+				"IF0511", // -d
+				110, // -targetCol
+				null, // -targetValue
+				true// -l
+		);
+		target.exec();
+		Assert.assertEquals(0, target.getMatchCount());
+		Assert.assertEquals(17, target.getUnMatchCount());
+		Assert.assertEquals(0, target.getNotFoundResCount());
+		Assert.assertEquals(0, target.getNotFoundExpCount());
+	}
+}
